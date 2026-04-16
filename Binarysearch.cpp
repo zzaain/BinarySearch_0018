6b978973cb326a8e59b2c5bbb853800d01114891
zzaain <sheriellaghzain@gmail.com>
2026-04-16 15:23:49 +0700

    menambahkan fungsi elemen array

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -24,6 +24,17 @@ void input()
     }
 
 
+    cout << "\n====================================================\n";
+    cout << "             Masukkan elemen array                    \n";
+    cout << "======================================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+
+}
 
 int main ()
 {
