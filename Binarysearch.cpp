d2ae94a994c8b1ef267223be8a84798d528f2d86
zzaain <sheriellaghzain@gmail.com>
2026-04-16 15:30:45 +0700

    menambahkan fungsi binary search

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -77,6 +77,51 @@ void display()
 }
 
 
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n=====================================\n";
+        cout << "    Pencarian Binary Search             \n";
+        cout << "=====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+        
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        } 
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+
+    } while (ulang == 'y' || ulang == 'Y');
+}
+
 int main ()
 {
     
