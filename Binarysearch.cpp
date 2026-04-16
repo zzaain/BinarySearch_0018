3bcd0c627686d417d7b6616041f7480b4f692766
zzaain <sheriellaghzain@gmail.com>
2026-04-16 15:27:10 +0700

    menambahkan proses bubble sort

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -38,7 +38,24 @@ void input()
 
 void bubbleSortArray()
 {
-    
+    {
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang -1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }  
+        pass++;
+    } while (pass < nPanjang - 1);
+}
+
+
 }
 
 int main ()
